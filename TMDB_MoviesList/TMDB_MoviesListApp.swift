293b8849a43d5c0396a03641c97efb1c959ef79c
ShawalAhsan-iOSDev <shawalahsan_2645@icloud.com>
2024-12-13 22:09:10 -0800

    Initial Commit

---
+++ TMDB_MoviesList/TMDB_MoviesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_MoviesListApp.swift
+//  TMDB_MoviesList
+//
+//  Created by mac os on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_MoviesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
